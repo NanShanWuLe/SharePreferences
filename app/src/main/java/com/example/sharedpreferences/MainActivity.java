b5af25d354f9324f6e7e371b9e579f58c1176361
xiaozuo <962894439@qq.com>
2021-04-06 23:22:45 +0800

    从SharedPreferences读取数据

--- app/src/main/java/com/example/sharedpreferences/MainActivity.java
+++ app/src/main/java/com/example/sharedpreferences/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ShareActionProvider;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button saveData = (Button)findViewById(R.id.save_data);
+        Button restoreData = (Button)findViewById(R.id.restore_data);
         saveData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -23,6 +25,18 @@ public class MainActivity extends AppCompatActivity {
                 editor.putInt("age", 28);
                 editor.putBoolean("married", false);
                 editor.apply();
+            }
+        });
+        restoreData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences pref = getSharedPreferences("data", MODE_PRIVATE);
+                String name = pref.getString("name", "");
+                int age = pref.getInt("age", 0);
+                boolean married = pref.getBoolean("married", false);
+                Log.d("TAG", "Name = " + name);
+                Log.d("TAG", "Age = " + age);
+                Log.d("TAG", "Married = " + married);
             }
         });
     }
